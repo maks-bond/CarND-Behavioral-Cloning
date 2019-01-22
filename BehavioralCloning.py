d5fb7b9c7f53bbec2e135cc063cdcf86d6fd56d7
Maksym Bondarenko <mabondaren@gmail.com>
2019-01-22 12:39:49 +0000

    Progress

--- BehavioralCloning.py
+++ BehavioralCloning.py
@@ -28,13 +28,22 @@ X_train = np.array(images)
 y_train = np.array(labels)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D
 
 model = Sequential()
-model.add(Flatten(input_shape=im_shape))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=im_shape))
+model.add(Conv2D(100, 5, 2, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(150, 5, 2, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(200, 5, 2, activation='relu'))
+model.add(Flatten())
+model.add(Dense(200))
+# Activation?
+model.add(Dense(50))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=15)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
 
 model.save('model.h5')
