d987e0ab790181af171da0c8800f7c2fe1128261
maks-bond <mabondaren@gmail.com>
2019-01-23 18:16:16 +0200

    Updated python file

--- BehavioralCloning.py
+++ BehavioralCloning.py
@@ -12,38 +12,60 @@ images = []
 labels = []
 im_shape = None
 
-print("Got ", len(lines), "training images")
-
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    img_path = './data1/IMG/'+filename
-    image = cv2.imread(img_path)
-    images.append(image)
-    im_shape = image.shape
-    label = float(line[3])
-    labels.append(label)
+    #for i in range(3):
+    if True:
+        i = 0
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        img_path = './data1/IMG/'+filename
+        image = cv2.imread(img_path)
+        images.append(image)
+        flipped_image = cv2.flip(image, 1)
+        images.append(flipped_image)
+
+        im_shape = image.shape
+        label = float(line[3])
+        
+        offset_angle = 0.2
+        if i == 1:
+            # left image
+            label += offset_angle
+        if i == 2:
+            # right image
+            label -= offset_angle
+        
+        labels.append(label)
+        labels.append(-1.0*label)
 
 X_train = np.array(images)
 y_train = np.array(labels)
 
+print("Got ", X_train.shape[0], "training images")
+print("Image shape is: ", im_shape)
+
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Cropping2D
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=im_shape))
-model.add(Conv2D(100, 5, 2, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Conv2D(150, 5, 2, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Conv2D(200, 5, 2, activation='relu'))
-model.add(Flatten())
-model.add(Dense(200))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((60, 20), (0, 0))))
+model.add(Conv2D(24, kernel_size=5, strides=2, padding='valid', activation='relu'))
+model.add(Conv2D(36, kernel_size=5, strides=2, padding='valid', activation='relu'))
+model.add(Conv2D(48, kernel_size=5, strides=2, padding='valid', activation='relu'))
+#model.add(MaxPooling2D())
+model.add(Conv2D(64, kernel_size=3, strides=2, padding='valid', activation='relu'))
+#model.add(MaxPooling2D())
+model.add(Conv2D(64, kernel_size=3, strides=2, padding='valid', activation='relu'))
+model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Dense(100))
 # Activation?
 model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
+model.summary()
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
 
 model.save('model.h5')
